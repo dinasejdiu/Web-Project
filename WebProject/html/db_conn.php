5f7a391908337ab68d3770c80eb8fcd3bd7a29ca
fortunagashi <95768944+fortunagashi@users.noreply.github.com>
2022-03-01 00:15:58 +0100

    Create db_conn.php
    shtimi i db_conn.php ne folder html

---
+++ WebProject/html/db_conn.php
@@ -0,0 +1,16 @@
+<?php
+$sname="localhost";
+$unamee="root";
+$password="";
+$db_name="test";
+
+
+
+$conn=mysqli_connect($sname,$unamee,$password,$db_name);
+
+if(!$conn)
+{
+	echo "Connection failed!";
+}
+
+?>
